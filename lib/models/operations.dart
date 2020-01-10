938e6ccf300cc70aea9865885d0975c0d815798e
Gustin Florian <florian.gustin.business@gmail.com>
2020-01-10 18:14:51 +0100

    [ADD] operations model

--- lib/models/operations.dart
+++ lib/models/operations.dart
@@ -1,5 +1,9 @@
 class Operations{
 
-  allClear(String numberString) => 0;
-
+  pourcentage({num nb}) => nb/100;
+  divide({num nbLeft, num nbRight}) => nbLeft / nbRight;
+  multiply({num nbLeft, num nbRight}) => nbLeft * nbRight;
+  substract({num nbLeft, num nbRight}) => nbLeft - nbRight;
+  addition({num nbLeft, num nbRight}) => nbLeft + nbRight;
+  
 }
